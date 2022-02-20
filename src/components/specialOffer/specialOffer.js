d44bb6cb70d87a0b2b49ef8ad2f9bc90431c1f4e
Hanna <iannika7@gmail.com>
2022-02-20 12:47:31 +0300

    edit class

--- src/components/specialOffer/specialOffer.js
+++ src/components/specialOffer/specialOffer.js
@@ -12,12 +12,12 @@ const SpecialOffer = () => {
                         <span className="title">Special Offer</span>
                         <div className="text">Subscribe <br/> And <span style={{color: '#E91E63'}}>Get 10% Off</span></div>
                     </div>
-                    <form className="subscribe-form_form">
+                    <form className="special-offer-form_form">
                         <input typeof="text" placeholder="Enter your email"></input>
                         <button>Subscribe</button>
                     </form>
                 </div>
-                <div className="subscribe-img-man"></div>
+                <div className="special-offer-img-man"></div>
             </div>
         </div>
     )
